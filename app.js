06c473186937ca2f7f9c0ce302ead46000d99309
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-02 19:22:38 +0530

    webserver

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+
+const http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('Hello World!');
+res.end();
+}).listen(3000);
+
+
 fs.readFile('file.txt', 'utf8', function (err, data) {
 if (err) throw err;
 console.log(data);
@@ -6,8 +15,10 @@ console.log(data);
 
 
 
-const fs = require('fs');
+
 fs.writeFile('file.txt', 'Hello World!', function (err) {
 if (err) throw err;
 console.log('File saved!');
 });
+
+
