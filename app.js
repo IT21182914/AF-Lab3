748bed4ab4ce91fd29d8ed6f2964170389a2b03a
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-02 19:27:04 +0530

    http request

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ res.write('Hello World!');
 res.end();
 }).listen(3000);
 
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+let data = '';
+resp.on('data', (chunk) => {
+data += chunk;
+});
+resp.on('end', () => {
+console.log(JSON.parse(data));
+});
+}).on('error', (err) => {
+console.log("Error: " + err.message);
+});
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
 if (err) throw err;
